291bc465fd80f5640cdda6228733d407a0427962
pthreat@github.com <pthreat@github.com>
2015-07-02 21:18:15 -0400

    Added tests folder

--- tests/type/StrDisk.test.php
+++ tests/type/StrDisk.test.php
@@ -9,6 +9,7 @@
 	$log		=	new Log();
 
 	$string	=	'aあ こんにちはaaakanaa';
+
 	$log->info("Base string: $string");
 	$str		=	new StrDisk($string);
 	$log->info("Length: %d",$str->strlen());
@@ -27,3 +28,15 @@
 
 	$log->info("Modified string: $str");
 
+	$log->info("Unset offset 1: $str[1]");
+
+	unset($str[1]);
+
+	$log->info("Value now is: $str");
+
+	$log->info("Check if an existing value (2) isset: %s",var_export(isset($str[2]),TRUE));
+	$log->info("Check if an unexisting value (22) isset: %s",var_export(isset($str[22]),TRUE));
+
+	$log->info("Try to substring, starting from 0 to 3");
+	$substr	=	$str->substr(0,2);
+	$log->info("Substring value: $substr");
